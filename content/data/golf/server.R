13838d02b3c3d903b9e2c297d42709c5166211dc
rockandroh <metrics@kakao.com>
2020-11-26 07:35:39 -0600

    update

--- content/data/golf/server.R
+++ content/data/golf/server.R
@@ -1,15 +1,124 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#'
+#' previous calendar week's Monday
+#' 날짜 뽑아내기
+#'
 
-library(shiny)
+library('shiny')
+library('DT')
+library('lubridate')
 
-# Define server logic required to draw a histogram
+club_name = c('군위구니','군위오펠','그레이스','마스터피스',
+              '세븐밸리','힐마루')
+
+#' Calculate date function 
+printf <- function(...) cat(sprintf(...))
+
+prev_n_weekday = function(date, wday, n=1, start = 'Sun'){
+    date = as.Date(date)
+    if (start == 'Sat'){
+        k = 6
+    }
+    else if(start == 'Fri'){
+        k = 5
+    }
+    else if(start == 'Sun'){
+        k = 7
+    }
+    else if(start == 'Mon'){
+        k = 1
+    }
+    diff = wday - wday(date, week_start = k)
+    diff = diff - 7*n
+    mydate = date + diff
+    return(mydate)
+}
+
+# 청도 그레이스
+grace = function(date){
+    date = as.Date(date)
+    mydate = prev_n_weekday(date, wday(date), 2)
+    return(mydate)
+}
+
+# 군위 군위오펠
+ophel = function(date){
+    date = as.Date(date)
+    wday = 3 # 화요일
+    # start 세팅값 7=토요일 // 1=일요일
+    if(wday(date) == 7){
+        mydate = prev_n_weekday(date, wday, 1, start = 'Sun')
+    }
+    # 주중
+    else {
+        mydate = prev_n_weekday(date, wday, 2, start = 'Sun')
+    }
+    return(mydate)
+}
+
+# 군위 구니cc
+guni = function(date){
+    date = as.Date(date)
+    wday = 3
+    # start 세팅값 7=토요일 // 1=일요일
+    if(wday(date) == 7){
+        mydate = prev_n_weekday(date, wday, 2, start = 'Sun')
+    }
+    else if(wday(date) == 1){
+        mydate = prev_n_weekday(date, wday, 3, start = 'Sun')
+    }
+    # 주중
+    else {
+        mydate = prev_n_weekday(date, wday, 2, start = 'Sun')
+    }
+    return(mydate)
+}
+
+# 창녕 동훈힐마루 (비회원기준)
+hilmaru = function(date){
+    date = as.Date(date)
+    
+    # start 세팅값 7=토요일 // 1=일요일
+    if(wday(date) == 7){
+        mydate = prev_n_weekday(date, wday = 6, 3, start = 'Sun')
+    }
+    else if(wday(date) == 1){
+        mydate = prev_n_weekday(date, wday = 6, 4, start = 'Sun')
+    }
+    # 주중
+    else {
+        mydate = prev_n_weekday(date, wday = 3, 3, start = 'Sun')
+    }
+    
+    return(mydate)
+}
+
+# 세븐밸리
+valley = function(date){
+    date = as.Date(date)
+    wday = 3 # 화요일
+    # start 세팅값 7=토요일 // 1=일요일
+    if(wday(date) == 7){
+        mydate = prev_n_weekday(date, wday, 1, start = 'Sun')
+    }
+    # 주중
+    else {
+        mydate = prev_n_weekday(date, wday, 2, start = 'Sun')
+    }
+    return(mydate)
+}
+
+# 마스터피스
+master = function(date){
+    date = as.Date(date)
+    mydate = prev_n_weekday(date, wday(date), 3)
+    return(mydate)
+}
+
+#' -------------------------------------------------
+#' Define server logic required to draw a histogram
+#' 서버
+#' -------------------------------------------------
+
 shinyServer(function(input, output, session) {
 
     output$seldate = renderText ({
@@ -21,11 +130,39 @@ shinyServer(function(input, output, session) {
         # paste("Selected Date is ", format(input$date, "%m/%d/%y"))
     })
     
-
+    data <- eventReactive(input$date, {rbind(format(input$date, "%Y-%m-%d"),
+                                        format(ophel(input$date), "%Y-%m-%d"),
+                                        format(guni(input$date), "%Y-%m-%d"))
+                                      }
+                          )
     
-    # for display of mtcars dataset in the "Data Page"
-    output$data <- renderTable({
-        mtcars
+    data2 <- eventReactive(input$date, {data.frame('골프장'= club_name,
+                                                   '오픈날짜'= c(format(guni(input$date), "%m월 %d일"), 
+                                                                format(ophel(input$date), "%m월 %d일"),
+                                                                format(grace(input$date), "%m월 %d일"), 
+                                                                format(master(input$date), "%m월 %d일"),
+                                                                format(valley(input$date), "%m월 %d일"), 
+                                                                format(hilmaru(input$date), "%m월 %d일")
+                                                              ),
+                                                   '시간' = c('10시','10시','9시','9시','9시','9시')
+                                                   )
+                                        }
+                          )
+    
+    mydata = eventReactive(input$date, {cbind(c(as.character(input$date), as.character(ophel(input$date), "%Y-%m-%d")),
+                                              c(as.character(guni(input$date), "%Y-%m-%d")), as.character(guni(input$date), "%Y-%m-%d"))
+                                        }
+                          )
+  
+    # output$test3 = DT::renderdatatable({data.frame('골프장'=club_name[1:3],
+    #                                     'date' = data2)()})
+    output$test3 = DT::renderDataTable({data2()})
+    
+    final_df = eventReactive(input$searchbutton, {
+        datatable(mtcars[,1:4], class = 'hover') 
     })
+    
+    output$mytable = DT::renderDataTable({final_df()}) 
+
 
 })
--- content/data/golf/ui.R
+++ content/data/golf/ui.R
@@ -8,17 +8,9 @@
 #
 
 library(shiny)
+library(DT)
 
-# Navigation through the tabs at the top of the page
-# These navigation tabs may server as individual pages, may have contents or widgets or further menu based navigation
-
-# use inverse = T , for black background of nav bar menu and light fonts
-# use selected argument to specify which tab should show up when app loads
-# use position="fixed-bottom" - navbar at the bottom
-# use position="static-top" - for scrollable bar
-# use position="fixed-top" - bar is fixed but page is scrollable
-# use collapsible = T if the app is used on tab where resolution is less. It will appear as collapsible menu
-# Important note that a navbarmenu cannot be the first tab/page
+linebreaks <- function(n){HTML(strrep(br(), n))}
 
 shinyUI(navbarPage(title = "공치자", 
                    
@@ -31,36 +23,41 @@ shinyUI(navbarPage(title = "공치자",
                             h4(paste("오늘은", format(Sys.Date(), "%Y년 %m월 %d일"), " 입니다.")),
                             textOutput("today"),
                             column(12,
-                                   dateInput("date",  # Input ID
-                                      
-                                      #label="Date Input", # label
-                                      # use below to show the calendar icon inline
-                                      label = span(HTML("<i class='glyphicon glyphicon-calendar'></i> 아래의 박스를 클릭하여 날짜를 선택하세요!"),
-                                                   style="color:red"),
-                                      language = "kr",
-                                      value = Sys.Date(), # date value that shows up initially
-                                      min = Sys.Date(),  # set the minimun date
-                                      max = Sys.Date() + 30, # set the maximum date
-                                      width = "300px", # set the width of widget
-                                      
-                                      format="yyyy-mm-dd") # set the format (default is yyyy-mm-dd)
-                            
+                               dateInput("date",  # Input ID
+                              #label="Date Input", # label
+                              # use below to show the calendar icon inline
+                              label = h4(span(HTML("<i class='glyphicon glyphicon-calendar'></i> 아래의 날짜박스를 클릭하여 날짜를 선택하세요!"),
+                                           style="color:red")),
+                              language = "kr",
+                              value = Sys.Date()+1, # date value that shows up initially
+                              min = Sys.Date(),  # set the minimun date
+                              max = Sys.Date() + 30, # set the maximum date
+                              width = "450px", # set the width of widget
+                              autoclose = FALSE,
+                              format="yyyy-mm-dd") # set the format (default is yyyy-mm-dd)
                             ),
+                            br(),
                             h4(span(textOutput("seldate"), style = "color:blue")),
                             hr(),
-                            column(3 , h4("날짜 선택 후 조회 버튼을 눌러주세요")),
-                            column(5, actionButton(inputId = "searchbutton", "조회")),
+                            #column(8, h4("날짜 선택 후 초록색 조회 버튼을 눌러주세요!")),
+                            #column(4, actionButton(inputId = "searchbutton", "조회", icon = icon("mouse-pointer"), class = "btn-success")),
+                            #hr(),
+                            #linebreaks(1),
+                            textOutput('test'),
+                            textOutput('test2'),
+                            DT::dataTableOutput("test3"),
+                            linebreaks(2),
+                            DT::dataTableOutput("mytable")
+                            ),
+                   
+                   tabPanel("골프장별 정보",
+                            h4("아래의 주소를 클릭하시면 정리된 골프장 정보를 확인할 수 있습니다."),
+                            h1(a("클릭", href = "https://www.rockandroh.com/data/golf-club-summary/"))
+                            ),
                             
-                            h5('test')
-                   ),
-                   
-                   
-                   
-                   tabPanel("골프장별 정보", 
-                            tableOutput("data")),
-                   
                    tabPanel("골프 명언",
-                            h5("Success in this game depends less on strength of body than strength of mind and character. - 아놀드파머 "),
-                            h5("You must play boldly to win. - 아놀드파머 "))
+                            h3("- Success in this game depends less on strength of body than strength of mind and character. [Arnold Palmer] "),
+                            h3("- You must play boldly to win. [Arnold Palmer] ")
+                            )
                    
 ))
